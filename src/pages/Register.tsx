789c2ea4e96db48509dbf79b11c3dc2d7ee0e02b
Matthew Pandolfo <matt@pandolfo.com>
2023-04-02 21:26:43 -0400

    form validate

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,17 +1,56 @@
+import * as Yup from "yup";
 import { Button, PasswordInput, Text, TextInput } from "@mantine/core";
+import { isEmail, useForm, yupResolver } from "@mantine/form";
+
+const schema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string()
+    .required("Password is required")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+      "Does not meet requirements"
+    ),
+  username: Yup.string().required().min(1),
+});
 
 function Register() {
+  const form = useForm({
+    validate: yupResolver(schema),
+    validateInputOnChange: true,
+    initialValues: {
+      email: "",
+      password: "",
+      confirmPass: "",
+      username: "",
+    },
+  });
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+  };
+
   return (
     <>
-      <form>
-        <TextInput withAsterisk label="Email" />
-        <PasswordInput
-          withAsterisk
-          label="Password"
-          description="Must include at least one letter, number and special character"
-        />
-        <PasswordInput withAsterisk label="Confirm Password" />
-        <Button>Register</Button>
+      <form onSubmit={handleSubmit}>
+        <>
+          <TextInput
+            withAsterisk
+            label="Email"
+            {...form.getInputProps("email")}
+          />
+          <TextInput
+            withAsterisk
+            label="Username"
+            {...form.getInputProps("username")}
+          />
+          <PasswordInput
+            withAsterisk
+            label="Password"
+            description="Must be at least 8 characters long and include at least one letter, number and special character"
+            {...form.getInputProps("password")}
+          />
+        </>
+        <Button type="submit">Register</Button>
       </form>
       <Text>
         Have an account?{" "}
